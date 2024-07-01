181ce2d2abc41038769d0ad8ba17f97e42de6bf7
Minha02 <fathumaminha@hmail.com>
2024-07-01 23:20:03 +0530

    Seccond commit

--- Act3.scala
+++ Act3.scala
@@ -4,7 +4,23 @@ object Act3 {
     else reverseString(str.tail) + str.head
   }
 
+  def filterLongStrings(strings: List[String]): List[String] = {
+  strings match {
+    case Nil => Nil
+    case head :: tail =>
+      if (head.length > 5) {
+        head :: filterLongStrings(tail)
+      } else {
+        filterLongStrings(tail)
+      }
+  }
+}
+
   def main(args: Array[String]): Unit = {
     println("Reversed String: " + reverseString("HELLO"))
+
+    val inputStrsings = List("Nimal", "Kamal", "BKNKNKnknas","Abcdefghijk")
+    val filteredStrings = filterLongStrings(inputStrsings)
+    println(filteredStrings)
   }
 }
